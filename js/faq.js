3309509a4bf739457b8e532b9d1628c655f227dc
BenjaValde <benj.valderrama@duocuc.cl>
2025-09-05 21:03:26 -0400

    Se agregan  el boceto de la seccion de noticias(html,css), se enlaza al menu

--- js/faq.js
+++ js/faq.js
@@ -1,7 +1,17 @@
 const questions = document.querySelectorAll('.faq-question');
-        questions.forEach(q => {
-            q.addEventListener('click', () => {
-                const item = q.parentElement;
-                item.classList.toggle('active');
-            });
+
+questions.forEach(q => {
+    q.addEventListener('click', () => {
+        const item = q.parentElement;
+
+        // Cierra las otras preguntas
+        document.querySelectorAll('.faq-item').forEach(i => {
+            if (i !== item) {
+                i.classList.remove('active');
+            }
         });
+
+        // Abre la pregunta actual
+        item.classList.toggle('active');
+    });
+});
